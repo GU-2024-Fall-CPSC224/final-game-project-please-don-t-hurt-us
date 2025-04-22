e903b6205e57dec3550d58057b8c1737710a8eb3
jtruong2004 <maindirect.jason@gmail.com>
2025-04-22 13:54:36 -0700

    finished coords

--- src/main/java/edu/gonzaga/Coordinate.java
+++ src/main/java/edu/gonzaga/Coordinate.java
@@ -4,25 +4,38 @@ public class Coordinate {
     char row;
     int column;
 
-    void isEqual(Coordinate coordinate) {
+    public Coordinate(char row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public boolean isEqual(Coordinate coordinate) {
         // Check if the current coordinate is equal to the given coordinate
         return this.row == coordinate.row && this.column == coordinate.column;
     }
 
-    void isValid() {
+    public boolean isValid() {
         // Check if the coordinate is valid (within the bounds of the board)
         return row >= 'A' && row <= 'J' && column >= 1 && column <= 10;
     }
 
-    void toString() {
+    public String toString() {
         // Convert the coordinate to a string representation
         return String.format("%c%d", row, column);
     }
 
-    void fromString(String coordinateString) {
+    public void fromString(String coordinateString) {
         // Convert a string representation of a coordinate to a Coordinate object
         this.row = coordinateString.charAt(0);
         this.column = Integer.parseInt(coordinateString.substring(1));
     }
-    
+
+    public char getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
 }
+
